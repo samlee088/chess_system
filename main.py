a908ab69bd4510f72a666f769f1cc655d8c2181c
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-08-01 22:04:48 -0700

    square class

--- main.py
+++ main.py
@@ -3,3 +3,17 @@ class Color(Enum):
     WHITE = 1
     BLACK = 2
 
+class Square:
+    def __init__(self, color):
+        self.color = color
+        self.piece = None
+    
+    def get_piece(self):
+        return self.piece
+
+    def get_color(self):
+        return self.color
+    
+    def set_piece(self, piece):
+        self.piece = piece
+
