1c3ad5a9decde0d7982dbbc2ebcdb10d7dd934e5
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-08-01 22:08:37 -0700

    piece class

--- main.py
+++ main.py
@@ -17,3 +17,23 @@ class Square:
     def set_piece(self, piece):
         self.piece = piece
 
+class Piece(ABC):
+    
+    def __init__(self, color):
+        self.color = color
+    
+    def get_color(self):
+        return self.color
+
+    @staticmethod
+    def is_within_grid(end_row, end_col):
+        return 0 <= end_row <= 7 and 0 <= end_col <= 7
+    
+    @abstractmethod
+    def is_valid_move(self, start_row, start_col, end_row, end_col, board):
+        pass
+
+    @abstractmethod
+    def get_symbol(self):
+        pass
+
